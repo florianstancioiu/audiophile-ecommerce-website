d1e9c7880d5c6acfda6aecfa06873314dddb0f52
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-04-28 11:22:51 +0300

    Worked on mobile categories for home

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,48 @@
 import classes from './Categories.module.css';
+import Category from './Category';
 
-const Categories = () => {};
+import HeadphonesCategoryImg from '../images/home/mobile/category-headphones.png';
+import SpeakersCategoryImg from '../images/home/mobile/category-speakers.png';
+import EarphonesCategoryImg from '../images/home/mobile/category-earphones.png';
+
+const categories = [
+  {
+    id: 1,
+    title: 'Headphones',
+    image: HeadphonesCategoryImg,
+    imageAltText: 'Headphones Category Image',
+    link: '/categories/headphones',
+  },
+  {
+    id: 2,
+    title: 'Speakers',
+    image: SpeakersCategoryImg,
+    imageAltText: 'Speakers Category Image',
+    link: '/categories/speakers',
+  },
+  {
+    id: 3,
+    title: 'Earphones',
+    image: EarphonesCategoryImg,
+    imageAltText: 'Earphones Category Image',
+    link: '/categories/earphones',
+  },
+];
+
+const Categories = () => {
+  return (
+    <div className={classes.wrapper}>
+      {categories.map((category) => (
+        <Category
+          key={category.id}
+          title={category.title}
+          image={category.image}
+          imageAltText={category.imageAltText}
+          link={category.link}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Categories;
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,5 +1,19 @@
 import classes from './Category.module.css';
+import ShopArrowIcon from '../images/icon-shop-arrow.svg';
 
-const Category = () => {};
+const Category = ({ image, imageAltText, title, link }) => {
+  return (
+    <div className={classes.wrapper}>
+      <img className={classes.image} src={image} alt={imageAltText} />
+      <div className={classes['inner-wrapper']}>
+        <p className={classes.title}>{title}</p>
+        <a className={classes.link} url='#'>
+          <span>Shop</span>
+          <img src={ShopArrowIcon} alt='' />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Category;
