2b44724c325e11767372a462b6396e6fabe1912c
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-05-02 11:06:56 +0300

    Worked on ZX7 speaker mobile section

--- src/components/SeeProductZX7.jsx
+++ src/components/SeeProductZX7.jsx
@@ -1,5 +1,15 @@
 import classes from './SeeProductZX7.module.css';
+import Button from './UI/Button';
 
-const SeeProductZX7 = () => {};
+const SeeProductZX7 = () => {
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes['inner-wrapper']}>
+        <h3 className={classes.title}>ZX7 SPEAKER</h3>
+        <Button title='See Product' type='ternary' />
+      </div>
+    </div>
+  );
+};
 
 export default SeeProductZX7;
