9fe842ecba7d33b60bf0d6b5389aa6897bd4e8d1
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-05-02 11:22:19 +0300

    Worked on ProductYX1 mobile section

--- src/components/SeeProductYX1.jsx
+++ src/components/SeeProductYX1.jsx
@@ -1,5 +1,22 @@
 import classes from './SeeProductYX1.module.css';
+import YX1EarphonesImage from '../images/home/mobile/yx1-earphones.png';
+import Button from './UI/Button';
 
-const SeeProductYX1 = () => {};
+const SeeProductYX1 = () => {
+  return (
+    <div className={classes.wrapper}>
+      <img
+        className={classes.image}
+        src={YX1EarphonesImage}
+        alt='YX1 Earphones'
+      />
+
+      <div className={classes['inner-wrapper']}>
+        <h3 className={classes.title}>YX1 EARPHONES</h3>
+        <Button title='See Product' type='ternary' />
+      </div>
+    </div>
+  );
+};
 
 export default SeeProductYX1;
