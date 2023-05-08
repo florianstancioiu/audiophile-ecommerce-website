c236e3d49a9d52b0267b19c9b4c461b034c027fa
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-05-08 10:47:50 +0300

    Worked on ProductBox and ProductFeatures

--- src/components/ProductBox.jsx
+++ src/components/ProductBox.jsx
@@ -1,5 +1,17 @@
 import classes from './ProductBox.module.css';
 
-const ProductBox = () => {};
+const ProductBox = ({ box }) => {
+  return (
+    <div className={classes.wrapper}>
+      <h3 className={classes.title}>In the box</h3>
+      {box.map((item) => (
+        <div key={item.id} className={classes.item}>
+          <span className={classes['item-quantity']}>{item.count}x</span>
+          <span className={classes['item-title']}>{item.title}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductBox;
--- src/components/ProductFeatures.jsx
+++ src/components/ProductFeatures.jsx
@@ -1,5 +1,12 @@
 import classes from './ProductFeatures.module.css';
 
-const ProductFeatures = () => {};
+const ProductFeatures = ({ features }) => {
+  return (
+    <div className={classes.wrapper}>
+      <h3 className={classes.title}>Features</h3>
+      <p className={classes.description}>{features}</p>
+    </div>
+  );
+};
 
 export default ProductFeatures;
